5af875a4867199dfa532f34c8a6a315b5aca0e40
blank_name <>
2017-07-30 18:50:33 -0400

    ...

--- rplugin/python3/denite/source/output.py
+++ rplugin/python3/denite/source/output.py
@@ -80,7 +80,8 @@ class Source(Base):
         )
         syn_dot_num = (
             # r'syntax match vimNumber /\d\+\.(\.|\d*)\+/'
-            r'syntax match vimNumber /\d\+\.(\d\+(\.)\?)*/'
+            # r'syntax match vimNumber /\d\+\.(\d\+(\.)\?)*/'
+            r'syntax match vimNumber /\d\+(\.)\?/'
         )
         self.vim.command(syn_var_name)
         self.vim.command(syn_hash_num)
