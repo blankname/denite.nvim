6997523657a5597f6f52e6e14ea46e8ce2fedb30
blank_name <>
2017-07-31 16:33:34 -0400

    ...

--- rplugin/python3/denite/source/output.py
+++ rplugin/python3/denite/source/output.py
@@ -73,7 +73,8 @@ class Source(Base):
         syn_var_name = (
             # 'syntax match vimVar /^\h[a-zA-Z0-9#_]*\>/' +
             r'syntax match vimVar /^\s*\S\+\ze /' +
-            ' nextgroup=allThings' +
+            # ' nextgroup=allThings' +
+            ' nextgroup=@vimNumber,@vimOperGroup' +
             # r'syntax match allThings /.*/' +
             # ' contains=@vimOperGroup' +
             # ' nextgroup=@vimOperGroup' +
@@ -87,11 +88,11 @@ class Source(Base):
 
         # is there a more elegant way to do an include like this?
         # why isn't vimFuncName working
-        syn_include = (
-            r'syntax match allThings /.*/' +
-            ' contains=@vimNumber,@vimOperGroup,vimFuncName' +
-            ' contained'
-        )
+        # syn_include = (
+        #     r'syntax match allThings /.*/' +
+        #     ' contains=@vimNumber,@vimOperGroup,vimFuncName' +
+        #     ' contained'
+        # )
 
         # syn_cluster = (
         #     'syntax cluster vimOperGroup contains=@vimOperGroup'
@@ -122,7 +123,7 @@ class Source(Base):
             r'syntax match vimNumber /\d\+\(\.\d\+\)\+\ze$/'
         )
         self.vim.command(syn_var_name)
-        self.vim.command(syn_include)
+        # self.vim.command(syn_include)
         self.vim.command(syn_hash_num)
         self.vim.command(syn_dot_num)
 
