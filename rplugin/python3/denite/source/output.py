5575d9ea962073c1c5d182985af304aee30005e0
blank_name <>
2017-07-30 21:37:05 -0400

    ...

--- rplugin/python3/denite/source/output.py
+++ rplugin/python3/denite/source/output.py
@@ -73,7 +73,7 @@ class Source(Base):
         syn_var_name = (
             # 'syntax match vimVar /^\h[a-zA-Z0-9#_]*\>/' +
             r'syntax match vimVar /^\s*\S\+\ze /' +
-            ' nextgroup=@vimFunc' +
+            ' nextgroup=@vimFilter' +
             # ' nextgroup=@vimOperGroup,@vimFuncName' +
             # ' nextgroup=@vimFuncBodyList,@vimFuncList' +
             ' skipwhite'
