59c4c79787c05aa1085a05d486f96a9a93824719
blank_name <>
2017-07-30 21:07:27 -0400

    ...

--- rplugin/python3/denite/source/output.py
+++ rplugin/python3/denite/source/output.py
@@ -68,12 +68,11 @@ class Source(Base):
     def define_syntax_for_let(self):
         # need string, list, dictionary
         # self.vim.command('set syntax=vim')
-        self.vim.command("syntax match LonelyQuote /'/")
         self.vim.command('syntax include syntax/vim.vim')
         syn_var_name = (
             # 'syntax match vimVar /^\h[a-zA-Z0-9#_]*\>/' +
             r'syntax match vimVar /^\s*\S\+\ze /' +
-            ' nextgroup=@vimString,@vimOperGroup' +
+            ' nextgroup=@vimStringGroup,@vimOperGroup' +
             ' skipwhite'
         )
 
