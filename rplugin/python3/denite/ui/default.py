252840ed07ac1767a808bbcfbff0cd00ef13a222
blank_name <>
2017-07-31 22:07:52 -0400

    wrap long lines instead of truncating

--- rplugin/python3/denite/ui/default.py
+++ rplugin/python3/denite/ui/default.py
@@ -193,7 +193,7 @@ class Default(object):
             'number': False,
             'relativenumber': False,
             'winfixheight': True,
-            'wrap': False,
+            'wrap': True,
         }
         if self._context['cursorline']:
             window_options['cursorline'] = True
@@ -426,7 +426,7 @@ class Default(object):
         encoding = self._context['encoding']
         abbr = candidate.get('abbr', candidate['word']).encode(
             encoding, errors='replace').decode(encoding, errors='replace')
-        terms.append(abbr[:int(self._context['max_candidate_width'])])
+        terms.append(abbr)
         return (self._context['selected_icon']
                 if index in self._selected_candidates
                 else ' ') + ' '.join(terms)
